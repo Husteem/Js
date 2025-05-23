1070f516a153a06eb1aff3020d5aefc3299e257a
husteem <muhdnaimhussain@gmail.com>
2025-05-23 17:18:07 +0100

    Refactor multiply function and clean up code structure

--- func.js
+++ func.js
@@ -10,15 +10,44 @@ function add(num1, num2) {
 let result = add(10, 20);
 console.log(result);
 
-function multiply(num1, num2) {
-    return " Multiplication table for 3 is ", num1 * num2;
-    let num1 = 3;
-    let num2 = 1;
-    for (let i = 1; i * num1 <= 30; i++) {
-        console.log(num1, "*", i, "=", i * num1);
-        if (i == 5) {
-            break;
-        }
+function multiply(num1, ) {
+    console.log("Multiplication table for", num1);
+    for (let i = 1; i <= 30; i++) {
+        console.log(num1, "*", i, "=", num1 * i);
     }
-
 }
+multiply(1);
+multiply(2);
+multiply(3);
+multiply(4);
+multiply(7);
+multiply(5);
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+let adition = add(10, 20);
+console.log(adition);
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+console.log(subtract(20, 10));
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiply(10, 20));
+
+function divide(num1, num2) {   
+    return num1 / num2;
+}   
+console.log(divide(20, 10));
+
+function modulus(num1, num2) {
+    return num1 % num2;
+}   
+console.log(modulus(20, 10));
+
+    
+   
